aa3b867dba1e2e71088304fa6493e03032d4ac75
stoptherock1 <stoptherock1@gmail.com>
2020-07-20 23:07:03 +1000

    Initial commit

---
+++ src/test/java/by/brest/chucknorrisapp/ChuckNorrisAppApplicationTests.java
@@ -0,0 +1,13 @@
+package by.brest.chucknorrisapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuckNorrisAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
